2cfc7a396f37010a5b7a45ffe6dd4fed5e64706c
BJH <ben.holt+gitlab@mac.com>
2024-05-01 19:45:03 -0400

    Smallmachine: improve context, integrate debug trace, re-introduce backtrace, improve error messages

--- smallmachine.py
+++ smallmachine.py
@@ -1,14 +1,15 @@
 # SmallMachine: Copyright © 2021-2024 Benjamin Holt - MIT License
 
+from collections import deque
 #####
 
 
 ###  State Machine Core  ###
 class StateMachine(object):
-    """A state machine engine that makes minimal assumptions but includes some nice conveniences and powerful extensibility.
+    """A state machine engine that makes minimal assumptions but includes a few nice conveniences and powerful extensibility.
     """
 
-    def __init__(self, rules, state, tracer=lambda fmt, **_: None):
+    def __init__(self, rules, state, tracer=False, history=10):
         """Create a state machine instance which can be called with input and returns output from evaluating the rules for the current state.
 
         The rules dictionary maps each state to a list of rule tuples, each of which includes a label, a test, an action, and a destination; more about rule elements in the __call__ documentation.
@@ -23,6 +24,7 @@ class StateMachine(object):
         self.rules = rules
         self.state = state
         self.tracer = tracer
+        self.history = deque(maxlen=history)
         self._input_count = 0
 
     def __call__(self, input):
@@ -38,41 +40,50 @@ class StateMachine(object):
 
         - Action: when a test succeeds, the action is called with context arguments, including 'result' from the test above; the action's response will be included in the context arguments for the tracer and returned by this call.
 
-        - Destination: finally, the machine will tranition to the destination state unless the destination is ... (Elipsis).
+        - Destination: finally, the machine will transition to the destination state unless the destination is ... (Ellipsis).
         """
         try:
             self._input_count += 1
-            rule_list = self.rules[self.state] + self.rules.get(..., [])
+            rule_list = self.rules.get(self.state, []) + self.rules.get(..., [])
             for l,t,a,d in rule_list:
-                ctx = {
+                context = {
                     "machine": self, "state": self.state, 
                     "input_count": self._input_count, "input": input,
                     "label": l, "test": t, "action": a, "dest": d,
                 }
-                result = t(**ctx)
+                result = t(**context)
                 if result:
-                    response = a(result=result, **ctx)
-                    self.tracer(
-                        "{input_count}: {state}('{input}') > {label}: {result} -- {response} --> {dest}",
-                        result=result, response=response, **ctx,
-                    )
+                    response = a(result=result, **context)
                     if d is not ...:
                         self.state = d
+                    self._trace(result=result, response=response, new_state=self.state, **context,)
+                    if self.state not in self.rules:
+                        raise RuntimeError(f"State '{self.state}' is not in the ruleset")
                     return response
             else:
-                raise ValueError(f"Unrecognized input '{input}'")
+                raise ValueError(f"State '{self.state}' did not recognize input {self._input_count}: '{input}'")
         except Exception as e:
-            note = f"StateMachine processing  {self._input_count}: {self.state}('{input}')"
-            e.add_note(note)
+            trace_lines = "\n  ".join(self.format_trace())
+            e.add_note(f"StateMachine Traceback (most recent last):\n  {trace_lines}\n{type(e).__name__}: {e}")
             raise
+
+    _transition_fmt = "{input_count}: {state}('{input}') > {label}: {result} -- {response} --> {new_state}"
+    def _trace(self, **context):
+        if self.tracer:  # TODO: consider reintroducing custom tracer etc.
+            print(f"T> {self._transition_fmt.format(**context)}")
+        self.history.append(context)
+
+    def format_trace(self):
+        """Returns a list of trace lines from the history of the machine's transitions."""
+        return [self._transition_fmt.format(**context) for context in self.history]
 #####
 
 
 ###  Tracing  ###
-def Debug(prefix="T>", printer=print):
-    """Prints tracepoint with a distinctive prefix and, optionally, to a separate destination than other output"""
-    def t(fmt, **vals):
-        msg = f"{prefix} {fmt.format(**vals)}" if prefix else fmt.format(**vals)
-        printer(msg)
-    return t
+# def Debug(prefix="T>", printer=print):
+#     """Prints tracepoint with a distinctive prefix and, optionally, to a separate destination than other output"""
+#     def t(fmt, **vals):
+#         msg = f"{prefix} {fmt.format(**vals)}" if prefix else fmt.format(**vals)
+#         printer(msg)
+#     return t
 #####
